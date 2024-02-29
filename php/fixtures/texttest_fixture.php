6aeeffa96c8878a62759fce0bd4a32fe53f2cb1a
daniazulai <dani@d2concepts.nl>
2024-02-29 11:03:40 +0100

    Make use of the type constants in the fixtures

--- php/fixtures/texttest_fixture.php
+++ php/fixtures/texttest_fixture.php
@@ -6,20 +6,20 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use GildedRose\GildedRose;
 use GildedRose\Item;
+use GildedRose\ItemType;
 
 echo 'OMGHAI!' . PHP_EOL;
 
 $items = [
-    new Item('+5 Dexterity Vest', 10, 20),
-    new Item('Aged Brie', 2, 0),
-    new Item('Elixir of the Mongoose', 5, 7),
-    new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-    new Item('Sulfuras, Hand of Ragnaros', -1, 80),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
-    // this conjured item does not work properly yet
-    new Item('Conjured Mana Cake', 3, 6),
+    new Item(ItemType::DEXTERITY_VEST, 10, 20),
+    new Item(ItemType::AGED_BRIE, 2, 0),
+    new Item(ItemType::ELIXER, 5, 7),
+    new Item(ItemType::SULFURAS, 0, 80),
+    new Item(ItemType::SULFURAS, -1, 80),
+    new Item(ItemType::BACKSTAGE_PASSES, 15, 20),
+    new Item(ItemType::BACKSTAGE_PASSES, 10, 49),
+    new Item(ItemType::BACKSTAGE_PASSES, 5, 49),
+    new Item(ItemType::CONJURED_CAKE, 3, 6),
 ];
 
 $app = new GildedRose($items);

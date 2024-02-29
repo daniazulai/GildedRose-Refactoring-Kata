ad207413b7d3a2e1fe3a5da1d4a12b321ee8c23e
daniazulai <dani@d2concepts.nl>
2024-02-29 10:57:57 +0100

    Test default items

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -6,15 +6,26 @@ namespace Tests;
 
 use GildedRose\GildedRose;
 use GildedRose\Item;
+use GildedRose\ItemType;
 use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
-    public function testFoo(): void
+    public function testDefaultItem(): void
     {
-        $items = [new Item('foo', 0, 0)];
+        $items = [new Item(ItemType::ELIXER, 1, 3)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertSame('fixme', $items[0]->name);
+        // At the end of each day our system lowers both values for every item
+        $this->assertSame($items[0]->sellIn, 0);
+        $this->assertSame($items[0]->quality, 2);
+        $gildedRose->updateQuality();
+        // Once the sell by date has passed, `Quality` degrades twice as fast
+        $this->assertSame($items[0]->sellIn, -1);
+        $this->assertSame($items[0]->quality, 0);
+        // The `Quality` of an item is never negative
+        $gildedRose->updateQuality();
+        $this->assertSame($items[0]->sellIn, -2);
+        $this->assertSame($items[0]->quality, 0);
     }
 }

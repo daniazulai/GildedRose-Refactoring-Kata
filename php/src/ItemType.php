1b73c4cc05a9184b4fbfa5180dc72386a1d28763
daniazulai <dani@d2concepts.nl>
2024-02-29 10:55:25 +0100

    ItemType class with constants of the all item types

---
+++ php/src/ItemType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace GildedRose;
+
+final class ItemType
+{
+    const AGED_BRIE = 'Aged Brie';
+    const BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+    const SULFURAS = 'Sulfuras, Hand of Ragnaros';
+    const CONJURED_CAKE = 'Conjured Mana Cake';
+    const DEXTERITY_VEST = '+5 Dexterity Vest';
+    const ELIXER = 'Elixir of the Mongoose';
+}

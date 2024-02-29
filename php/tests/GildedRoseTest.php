d732ebc61ad9ec909d3f3f4a36e11d57e6cdc0a2
daniazulai <dani@d2concepts.nl>
2024-02-29 10:58:39 +0100

    Test Aged Brie

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -28,4 +28,22 @@ class GildedRoseTest extends TestCase
         $this->assertSame($items[0]->sellIn, -2);
         $this->assertSame($items[0]->quality, 0);
     }
+
+    public function testAgedBrie(): void
+    {
+        $items = [new Item(ItemType::AGED_BRIE, 1, 47)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        // Aged Brie actually increases in `Quality` the older it gets
+        $this->assertSame($items[0]->sellIn, 0);
+        $this->assertSame($items[0]->quality, 48);
+        $gildedRose->updateQuality();
+        // Once the sell by date has passed, `Quality` increases twice as fast
+        $this->assertSame($items[0]->sellIn, -1);
+        $this->assertSame($items[0]->quality, 50);
+        // The `Quality` of an item is never more than `50`
+        $gildedRose->updateQuality();
+        $this->assertSame($items[0]->sellIn, -2);
+        $this->assertSame($items[0]->quality, 50);
+    }
 }
